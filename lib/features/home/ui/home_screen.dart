6118c67d4301f728a8b5ad56d36d231ec3e5a2b3
Sofia Montoya Bolivar <sofia09262001@gmail.com>
2023-12-17 15:29:14 -0500

    Feat home UX

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pet_home/core/router/router.dart';
+import 'package:pet_home/features/home/ui/widget/card.dart';
+import 'package:pet_home/ui/constants/font_constants.dart';
+import 'package:pet_home/ui/constants/palette.dart';
+import 'package:pet_home/ui/constants/pethome_icons.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({this.showBottomBar = true, super.key});
 
   final bool showBottomBar;
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,14 +27,106 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            OutlinedButton(
-              onPressed: () => context.push(RoutePath.adoptionAlert.path),
-              child: const Text('OPRIMIR'),
-            ),
-            const TextField(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () =>
+                          context.push(RoutePath.adoptionAlert.path),
+                      child: const Text('Dar en adopción'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Pethome.filter,
+                      color: Palette.primary00,
+                      size: 33,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Adopciones en Medellín',
+                    style:
+                        FontConstants.body1.copyWith(color: Palette.primary00),
+                  ),
+                  Text(
+                    'Ver más',
+                    style: FontConstants.caption2.copyWith(
+                      color: Palette.primary00,
+                      decoration: TextDecoration.underline,
+                      decorationColor: Palette.primary00,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                height: 250,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    homeCard(context),
+                    homeCard(context),
+                    homeCard(context),
+                    homeCard(context),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '¡Estas macotas buscan un hogar!',
+                    style:
+                        FontConstants.body1.copyWith(color: Palette.primary00),
+                  ),
+                  Text(
+                    'Ver más',
+                    style: FontConstants.caption2.copyWith(
+                      color: Palette.primary00,
+                      decoration: TextDecoration.underline,
+                      decorationColor: Palette.primary00,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                height: 250,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    homeCard(context),
+                    homeCard(context),
+                    homeCard(context),
+                    homeCard(context),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/home/ui/widget/card.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:pet_home/ui/constants/pethome_icons.dart';
+
+Card homeCard(BuildContext context) {
+  return Card(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    elevation: 0.5,
+    child: ClipRect(
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 3.5,
+            width: MediaQuery.of(context).size.width / 1.2,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://estaticos-cdn.prensaiberica.es/clip/823f515c-8143-4044-8f13-85ea1ef58f3a_16-9-discover-aspect-ratio_default_0.jpg',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 7,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Pethome.female),
+              SizedBox(
+                width: 5,
+              ),
+              Text('Firulais'),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
