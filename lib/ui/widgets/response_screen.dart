c948ac1b2549a461af81f63dff7a540ceedf4ddf
Sofia Montoya <sofia09262001@gmail.com>
2024-04-07 02:51:56 -0500

    Fix response screen design

--- lib/ui/widgets/response_screen.dart
+++ lib/ui/widgets/response_screen.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 import 'package:pet_home/ui/constants/font_constants.dart';
 import 'package:pet_home/ui/constants/palette.dart';
 import 'package:pet_home/ui/scaffold/custom_scaffold.dart';
@@ -56,19 +57,32 @@ class _ResponseScreenState extends State<ResponseScreen> {
       child: CustomScaffold(
         withAppbar: false,
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              widget.title,
-              style: FontConstants.subtitle1.copyWith(
-                color: widget.isError ? Palette.errorDark : Palette.primary00,
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/animations/response_screen_animation.gif',
+                    height: 200,
+                  ),
+                  Text(
+                    widget.title,
+                    style: FontConstants.subtitle1.copyWith(
+                      color: widget.isError
+                          ? Palette.errorDark
+                          : Palette.primary00,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(widget.message),
+                ],
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(widget.message),
             LargeButton(text: widget.buttonMsg, onPressed: onNextButton),
+            const SizedBox(height: 20),
           ],
         ),
       ),
