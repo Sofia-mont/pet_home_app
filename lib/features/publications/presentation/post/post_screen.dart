12bd35288a056c64a266ddd0b0432fbce0f857ff
Sofia Montoya <sofia09262001@gmail.com>
2024-05-02 02:13:26 -0500

    Fix responsive post screen

--- lib/features/publications/presentation/post/post_screen.dart
+++ lib/features/publications/presentation/post/post_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
@@ -62,31 +64,28 @@ class _PublicationScreenState extends ConsumerState<PostScreen>
                 ),
                 Expanded(
                   child: Container(
-                    margin: const EdgeInsets.fromLTRB(10, 80, 10, 0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    margin: const EdgeInsets.fromLTRB(10, 100, 10, 0),
+                    child: ListView(
                       children: [
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    widget.publication.postOwner.name,
-                                    style: FontConstants.body1
-                                        .copyWith(color: Palette.primaryDarker),
-                                  ),
-                                  Text(
-                                    'Dueño',
-                                    style: FontConstants.caption2
-                                        .copyWith(color: Palette.textMedium),
-                                  ),
-                                ],
-                              ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  widget.publication.postOwner.name,
+                                  style: FontConstants.body1
+                                      .copyWith(color: Palette.primaryDarker),
+                                ),
+                                Text(
+                                  'Dueño',
+                                  style: FontConstants.caption2
+                                      .copyWith(color: Palette.textMedium),
+                                ),
+                              ],
                             ),
                             Text(
                               'Publicado en \n${DateFormat('MMM d, yyyy', 'es_ES').format(widget.publication.createdAt)}',
@@ -95,65 +94,61 @@ class _PublicationScreenState extends ConsumerState<PostScreen>
                             ),
                           ],
                         ),
-                        const SizedBox(height: 20),
-                        Expanded(
-                          child: Text(
-                            widget.publication.petHistory,
-                            textAlign: TextAlign.justify,
-                            style: FontConstants.body2
-                                .copyWith(color: Palette.textMedium),
-                          ),
+                        const SizedBox(height: 10),
+                        Text(
+                          widget.publication.petHistory,
+                          textAlign: TextAlign.justify,
+                          style: FontConstants.body2
+                              .copyWith(color: Palette.textMedium),
                         ),
-                        if (!widget.isOwner) ...[
-                          const Spacer(),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: LargeButton(
-                              onPressed: () => ref
-                                  .read(customModalsProvider)
-                                  .showInfoDialog(
-                                    buildContext: context,
-                                    title: 'Mensaje para el posible adoptante',
-                                    content:
-                                        'Si decides que quieres en tu vida la compañía de una vida animal, debes ser consciente del compromiso que esto implica. La vida está llena de cambios y en los 15 años que vive en promedio un perro o un gato se van a presentar una infinidad de situaciones que deberás enfrentar de manera seria y responsable. \n Si decides adoptar un animal debes acogerlo como un miembro más de tu casa. Esto quiere decir que, pase lo que pase, el animal siempre será tenido en cuenta en las decisiones de la familia y por ningún motivo se desharán de él, como si fuera un objeto o un juguete. Ningún problema es excusa para abandonarlo, del mismo modo que no es excusa para abandonar a cualquier otro integrante de tu familia',
-                                    buttonText: 'Aceptar',
-                                    buttonAction: () {
-                                      ref.read(appRouterProvider).pop();
-                                      ref
-                                          .read(appRouterProvider)
-                                          .pushNamed(PersonalDataScreen.path);
-                                    },
-                                  ),
-                              text: 'Adoptame',
-                            ),
-                          ),
-                        ],
                       ],
                     ),
                   ),
                 ),
+                if (!widget.isOwner) ...[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 10,
+                    ),
+                    child: LargeButton(
+                      onPressed: () =>
+                          ref.read(customModalsProvider).showInfoDialog(
+                                buildContext: context,
+                                title: 'Mensaje para el posible adoptante',
+                                content:
+                                    'Si decides que quieres en tu vida la compañía de una vida animal, debes ser consciente del compromiso que esto implica. La vida está llena de cambios y en los 15 años que vive en promedio un perro o un gato se van a presentar una infinidad de situaciones que deberás enfrentar de manera seria y responsable. \n Si decides adoptar un animal debes acogerlo como un miembro más de tu casa. Esto quiere decir que, pase lo que pase, el animal siempre será tenido en cuenta en las decisiones de la familia y por ningún motivo se desharán de él, como si fuera un objeto o un juguete. Ningún problema es excusa para abandonarlo, del mismo modo que no es excusa para abandonar a cualquier otro integrante de tu familia',
+                                buttonText: 'Aceptar',
+                                buttonAction: () {
+                                  ref.read(appRouterProvider).pop();
+                                  ref
+                                      .read(appRouterProvider)
+                                      .pushNamed(PersonalDataScreen.path);
+                                },
+                              ),
+                      text: 'Adoptame',
+                    ),
+                  ),
+                ],
               ],
             ),
           ),
           SafeArea(
-            child: Container(
-              margin: const EdgeInsets.only(top: 30),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        ref.read(appRouterProvider).pop();
-                      },
-                      icon: const Icon(
-                        Icons.arrow_back_ios_rounded,
-                        color: Colors.white,
-                      ),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      ref.read(appRouterProvider).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios_rounded,
+                      color: Colors.white,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
