375a3e2ed8321e92e718c9690b8dc9735351f9eb
Sofia Montoya <sofia09262001@gmail.com>
2024-09-08 14:34:39 -0500

    Api URL

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,8 +1,8 @@
 enum EnvType { dev, prod }
 
 class AppConstants {
-  static const String baseURL = 'http://10.0.2.2:8080/api/v1';
-  //static const String baseURL = 'https://pet-home-api-production.up.railway.app/api/v1';
+  //static const String baseURL = 'http://10.0.2.2:8080/api/v1';
+  static const String baseURL = 'https://pet-home-api-production.up.railway.app/api/v1';
   static const isProd = bool.fromEnvironment('isProd');
   static const forceLogs = bool.fromEnvironment('forceLogs');
 
